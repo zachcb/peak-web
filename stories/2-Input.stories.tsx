6c140aed1f6d6118dd49c40ce202492220492ad8
zachcb <zach.c.barnes@gmail.com>
2019-11-22 11:57:05 -0700

    Fix import path for components

--- stories/2-Input.stories.tsx
+++ stories/2-Input.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import Input from "../components/atoms/Input";
+import Input from "components/atoms/Input";
 
 export default {
   title: "Button",
