f039988a6782282375abda45ee0d73fb28aab78f
zachcb <zach.c.barnes@gmail.com>
2019-11-23 15:59:04 -0700

    Add custom colors; Fix tailwind tsconfig error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
+    colors: {
+      primary: "#0be881",
+      secondary: "#ff5e57",
+      ...colors
+    },
     extend: {},
   },
   variants: {},
