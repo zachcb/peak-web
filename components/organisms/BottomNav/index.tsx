515d831af67d7ebb517716ab2ce0706dc761c699
zachcb <zach.c.barnes@gmail.com>
2019-11-23 16:00:29 -0700

    Add link to bottom nav

--- components/organisms/BottomNav/index.tsx
+++ components/organisms/BottomNav/index.tsx
@@ -1,47 +1,29 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCompass, faPlusCircle, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCompass, faPlusCircle, faUserCircle } from "@fortawesome/free-solid- svg-icons";
 import styles from "./styles.css";
 
 type Props = {
   isActive: boolean;
 };
 
-function BottomNav({ isActive = true }: Props) {
-  return (
-    isActive && (
-      <div
-        className={`absolute bottom-0 z-50 w-screen flex flex-wrap content-between text-center bg-white ${styles["bottom-nav"]}`}
-      >
-        <a
-          className="w-1/3"
-          href="/#"
-        >
-          <FontAwesomeIcon
-            icon={faCompass}
-            className="h-6 w-6 m-auto my-2 fill-current text-blue-900"
-          />
-        </a>
-        <a
-          className="w-1/3"
-          href="/#"
-        >
-          <FontAwesomeIcon
-            icon={faPlusCircle}
-            className="h-6 w-6 m-auto my-2 fill-current text-blue-900"
-          />
-        </a>
-        <a
-          className="w-1/3"
-          href="/#"
-        >
-          <FontAwesomeIcon
-            icon={faUserCircle}
-            className="h-6 w-6 m-auto my-2 fill-current text-blue-900"
-          />
-        </a>
-      </div>
-    )
+function BottomNav({ isActive }: Props) {
+  return (isActive && (
+  <div
+    className={`absolute bottom-0 z-50 w-screen flex flex-wrap content-between text-center bg-white ${styles["bottom-nav"]}`}
+  >
+    <div className="w-1/3">
+      <Link href="/discover">discover</Link>
+    </div>
+    <div className="w-1/3">
+      <Link href="/create">create</Link>
+    </div>
+    <div className="w-1/3">
+      <Link href="/profile">profile</Link>
+    </div>
+  </div>
+  )
   );
 }
 
