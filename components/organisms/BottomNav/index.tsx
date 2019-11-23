1c1ad7342686dd98bfdbda0367f2b9c0d3a4b2be
zachcb <zach.c.barnes@gmail.com>
2019-11-22 19:34:13 -0700

    Add font awesome; replace icons in bottom nav

--- components/organisms/BottomNav/index.tsx
+++ components/organisms/BottomNav/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import styles from "./styles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompass, faPlusCircle, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
-const BottomNav = () => (
+type Props = {
+
+}
+
+const BottomNav: React.FC<Props> = () => (
   <div className={`absolute bottom-0 z-50 w-screen flex flex-wrap content-between text-center bg-white ${styles["bottom-nav"]}`}>
-    <a className="w-1/3" href="/#">Discovery</a>
-    <a className="w-1/3" href="/#">Create</a>
-    <a className="w-1/3" href="/#">Profile</a>
+    <a className="w-1/3" href="/#">
+      <FontAwesomeIcon
+        icon={faCompass}
+        className="h-6 w-6 m-auto my-2 fill-current text-blue-900"
+      />
+    </a>
+    <a className="w-1/3" href="/#">
+      <FontAwesomeIcon
+        icon={faPlusCircle}
+        className="h-6 w-6 m-auto my-2 fill-current text-blue-900"
+      />
+    </a>
+    <a className="w-1/3" href="/#">
+      <FontAwesomeIcon
+        icon={faUserCircle}
+        className="h-6 w-6 m-auto my-2 fill-current text-blue-900"
+      />
+    </a>
   </div>
 );
 
